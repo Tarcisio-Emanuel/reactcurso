67cdde6718cbac395b481f76ee6433c978ca2687
Tarcisio <tarcisioemanoel10031@gmail.com>
2024-10-24 22:24:02 -0300

    1comit

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -5,6 +5,7 @@ import './home.css';
 
 export default function Home() {
     const [filmes, setFilmes] = useState([]);
+    const [loading, setLoading] = useState(true);
     
     useEffect(() => {
         async function loadFilmes() {
@@ -16,11 +17,20 @@ export default function Home() {
                 }
             })
             // quantidade de filmes vai ate 20
-            console.log(response.data.results.slice(0, 10))
+            // console.log(response.data.results.slice(0, 10))
             setFilmes(response.data.results.slice(0, 10))
+            setLoading(false)
         }
         loadFilmes()
     }, []);
+
+if(loading){
+    return(
+        <div className="loading">
+            <h2>Carregando filmes...</h2>
+        </div>
+    )
+}
 
     return (
         <div className="container">
--- src/routes.js
+++ src/routes.js
@@ -3,7 +3,9 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Filme from './pages/Filme';
 
+
 import Header from './components/Header';
+import Erro from './pages/Erro';
 
 export default function RoutesApp() {
     return(
@@ -14,7 +16,7 @@ export default function RoutesApp() {
 
             <Route path='/' element={<Home />} />
             <Route path='/filme/:id' element={<Filme />} />
-
+            <Route path='*' element={<Erro/>} />
 
         </Routes>
     </BrowserRouter>
